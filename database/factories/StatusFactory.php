7dacb56795a70a005ba99920979c7adf825fc736
KhanhVinh <khanhvinhb2@gmail.com>
2025-12-26 17:59:40 +0700

    test: add StatusService tests and enable Status factory

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Status>
+ */
+class StatusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+        public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'color' => $this->faker->hexColor(),
+            'is_check_due' => $this->faker->boolean(),
+        ];
+    }
+
+    /**
+     * State: is_check_due = true
+     */
+    public function checkDue(): static
+    {
+        return $this->state(fn () => [
+            'is_check_due' => 1,
+        ]);
+    }
+
+    /**
+     * State: is_check_due = false
+     */
+    public function noCheckDue(): static
+    {
+        return $this->state(fn () => [
+            'is_check_due' => 0,
+        ]);
+    }
+}
